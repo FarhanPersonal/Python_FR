744e0e29a20f40362751fa14acb4fecc6b4f40be
Farhan Rahman <frn.rahman@gmail.com>
2020-07-13 19:54:07 -0600

    Functions defined with keyword arguments

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-# My portfolio
+def add(firstNum, secondNum, thirdNum=0):
+    return firstNum + secondNum + thirdNum
 
-# This is my work.
+
+def subtract(largeNum, amountTobeSubtracted):
+    return largeNum-amountTobeSubtracted
+
+#print(add(firstNum=1, secondNum=2))
+
+
+print(subtract(largeNum=100, amountTobeSubtracted=5))
