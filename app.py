24c81a479434716c5cc4c2cda82babd1520ac50e
Farhan Rahman <frn.rahman@gmail.com>
2020-07-14 12:20:32 -0600

    Adding class level attributes

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 # Creating custom class 'Point'with constructors.
 class Point:
+    # Class level attribute
+    default_color = "red"
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -9,6 +12,15 @@ class Point:
 
 
 point = Point(1, 2)
+
+Point.default_color = "Yellow"
+
+anotherPoint = Point(3, 4)
+
+point.default_color = "blue"
+
+print(point.default_color)
+print(anotherPoint.default_color)
 
 # FR: Creating attributes/properties of point object outside the class
 point.z = 20
