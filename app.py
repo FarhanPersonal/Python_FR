947bf7c124b17af41c332149927ed25ccace9bad
Farhan Rahman <frn.rahman@gmail.com>
2020-07-16 16:56:17 -0600

    Creating and using 'module' named 'salesModule'.

--- app.py
+++ app.py
@@ -1,23 +1,3 @@
-from abc import ABC, abstractclassmethod
+from salesModule import Get_price, GetDateOfSale
 
-# Stream is an abstract class since it is derived from abstract base class called 'ABC'
-class Stream(ABC):
-
-    # To ensure all derived calss of 'Stream' class has a method named 'read()'
-    # we decorate the read() method with '@abstractclassmethod' decorator
-    @abstractclassmethod
-    def read(self):
-        #we put 'pass' so that we don't need to write anything more for the method
-        pass
-
-
-class MemoryStream(Stream):
-    def __init__(self):
-        self.title = "This is a memory stream"
-
-# We cannot derive MemoryStream from Stream class unless we implement the read() as we did below:
-    def read(self):
-        return self.title
-
-memoryStream = MemoryStream()
-print(memoryStream.read())
+print("The price is ", Get_price())
---
+++ salesModule.py
@@ -0,0 +1,12 @@
+# This module contains functions and classes related to the concept of sale
+
+def Get_price():
+    return 10
+
+def GetDateOfSale():
+    pass
+
+class Sale():
+    pass
+
+
