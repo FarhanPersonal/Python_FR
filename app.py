2afe8420a879b5cf559ed9421dedbb50b58759a2
Farhan Rahman <frn.rahman@gmail.com>
2020-07-17 18:54:40 -0600

    Made subpackage 'shopping' within package 'ecommerce'

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # FR: The procedure used below to import a module in Python is similar to 'using namespace' in C#
-from ecommerce import salesModule
+from ecommerce.shopping import salesModule
 import sys
 
 sale = salesModule.Sale()
