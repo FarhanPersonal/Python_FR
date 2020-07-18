81387213a14b5a19da8dd8e54fa3a40784fcd0b6
Farhan Rahman <frn.rahman@gmail.com>
2020-07-17 19:14:09 -0600

    To find name of package containing the module

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 # FR: The procedure used below to import a module in Python is similar to 'using namespace' in C#
 from ecommerce.shopping import salesModule
-import sys
 
 sale = salesModule.Sale()
 
-print(sale.madeBy)
+# To see information related to salesModule
+#print(dir(salesModule))
+
+# full name of module
+print(salesModule.__name__)
+
+# name of package containing the module
+print(salesModule.__package__)
+
+# address of the module file
+print(salesModule.__file__)
+
+
--- ecommerce/shopping/salesModule.py
+++ ecommerce/shopping/salesModule.py
@@ -1,5 +1,13 @@
 # This module contains functions and classes related to the concept of sale
 
+# using absolute import below
+from ecommerce.customer import contact
+
+# using relative import below
+#from ..customer import contact
+
+#contact.contact_customer()
+
 def Get_price():
     return 10
 
