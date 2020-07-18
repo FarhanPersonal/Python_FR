3a1ed974480c5c980fabd2f2ee96f0ca2c7f5fa9
Farhan Rahman <frn.rahman@gmail.com>
2020-07-18 12:48:21 -0600

    Seeing size of numpy array representing image obtained through using of imread() of opencv

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-insImage = cv.imread('data/Institute.jpg', 0)
+insImage = cv.imread('data/Institute.jpg')
+# See the size of the insImage which is a color image
+print("Shape of insImage obtained through imread is ", insImage.shape)
 
+# See the size of the henriImage which is a greyscale image
 henriImage = cv.imread('data/Henri.jpg', 0)
+print("Shape of henriImage obtained through imread is ", henriImage.shape)
+
 
 array = np.array([1,2,3])
-print(array)
+# print(array)
 
 print(type(array))
 print(type(insImage))
+# print("Shape of array is ", array.shape)
