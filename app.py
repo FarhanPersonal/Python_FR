f82868677bc103ab7d260098357d995ba50e594c
Farhan Rahman <frn.rahman@gmail.com>
2020-07-17 19:37:02 -0600

    Learning about Path class

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
-# FR: The procedure used below to import a module in Python is similar to 'using namespace' in C#
-from ecommerce.shopping import salesModule
+from pathlib import Path
 
-sale = salesModule.Sale()
+path = Path("ecommerce/__init__.py")
+print(path.exists())
 
-# To see information related to salesModule
-#print(dir(salesModule))
-
-# full name of module
-print(salesModule.__name__)
-
-# name of package containing the module
-print(salesModule.__package__)
-
-# address of the module file
-print(salesModule.__file__)
-
-
+print(path.is_file())
+print(path.is_dir())
