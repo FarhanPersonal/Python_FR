14a1b0d11a83a00364676bfa59309a532767737a
Farhan Rahman <frn.rahman@gmail.com>
2020-07-18 18:00:35 -0600

    # convert obtained color image to grey image by setting flag = 0

--- app.py
+++ app.py
@@ -4,33 +4,18 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-insImage = cv.imread('data/Institute.jpg')
+# convert obtained color image to grey image by setting flag = 0
+insImage_grey = cv.imread('data/Institute.jpg', 0)
+
+
 # See the size of the insImage which is a color image
-print("Shape of insImage obtained through imread is ", insImage.shape)
+cv.imshow('image', insImage_grey)
+cv.waitKey(0)
+
+
+# print(type(insImage))
 
 # See the size of the henriImage which is a greyscale image
 henriImage = cv.imread('data/Henri.jpg', 0)
-print("Shape of henriImage obtained through imread is ", henriImage.shape)
-
-
-array1 = np.zeros((3,4), dtype=int)
-
-array2 = np.ones((3,4), dtype=int)
-
-# creating array with a specific number
-array3 = np.full((3,4), 5, dtype=int)
-
-# creating array with random number
-array_random = np.random.random((3,4))
-
-print(array1)
-print(array2)
-print(array3)
-print(array_random)
-
-print(type(np))
-print(type(np.random))
-print(type(np.random.random()))
-
 
 
