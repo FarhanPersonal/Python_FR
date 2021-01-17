84dbbd4464dbc35a9f8277015babf43e947fef94
Farhan Rahman <frn.rahman@gmail.com>
2021-01-16 21:07:56 -0700

    Learning about list

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
-# FR: need to import cv2 like below otherwise doesn't work
-from cv2 import cv2 as cv   
-import numpy as np
+letters = ["a", "b", "c"]
 
-import matplotlib.pyplot as plt
+matrix = [[0, 1], [2, 3]]
 
-# convert obtained color image to grey image by setting flag = 0
-insImage_grey = cv.imread('data/Institute.jpg', 0)
+zeros = [0] * 100
 
-
-# See the size of the insImage which is a color image
-cv.imshow('image', insImage_grey)
-cv.waitKey(0)
-
-
-# print(type(insImage))
-
-# See the size of the henriImage which is a greyscale image
-henriImage = cv.imread('data/Henri.jpg', 0)
-
-
+print(zeros)
