4ab83e633a7fd554f00146b9a1853cd50d72a2af
Farhan Rahman <frn.rahman@gmail.com>
2021-01-17 12:49:34 -0700

    Converting list to a tuple

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 # Following mosh's lecture 5.15:
 
-# A tuple is a read-only list; we cannot modify a tuple
-
-# creating an empty tuple
-point = ()
-
-point = (1, 2) + (3, 4)
-
-print(type(point))
-
-print(point)
+pointArray = [1, 2]
+print(pointArray)
+pointTuple = tuple(pointArray)
+print(pointTuple)
