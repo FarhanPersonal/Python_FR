96ac21b54f39ca4ce0b47e9c6ddb037f11adf786
Farhan Rahman <frn.rahman@gmail.com>
2021-01-17 11:39:27 -0700

    If statement

--- app.py
+++ app.py
@@ -1,15 +1,4 @@
-letters = ["a", "b", "c"]
+letters = ["a", "b", "c", "d"]
 
-# adding objects to a list
-letters.append("d")
-
-letters.insert(0, "-")
-
-# removing method
-letters.pop()
-
-letters.pop(0)
-
-letters.remove("b")
-
-print(letters)
+if 'd' in letters:
+    print(letters.index("b"))
