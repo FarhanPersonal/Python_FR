6d43cf374a2575c604a2889b31c9ac78b6dbec71
Farhan Rahman <frn.rahman@gmail.com>
2021-01-17 13:38:29 -0700

    Looping over dictionary

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ point = dict(x=1, y=3)
 
 print(point["x"])
 print(point["y"])
+
+point["x"] = 10
+print(point["x"])
+
+# point["z"] = 30
+
+print(len(point))
+
+for key in point:
+    print(key, point[key])
