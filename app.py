5a1dd839ed013e24fd8b54cd76935e4146d4e511
Farhan Rahman <frn.rahman@gmail.com>
2021-01-17 12:15:49 -0700

    Lambda expression to create anonymous function

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-# Following mosh's lecture 5.7:
+# Following mosh's lecture 5.8:
 
 items = [("product1", 10), ("product2", 5), ("product3", 30)]
 
 
-def sort_items(item):
-    return item[1]
+# def sort_items(item):
+#     return item[1]
 
 
-items.sort(key=sort_items)
+items.sort(key=lambda item: item[1])
 
 print(items)
