f31e34ff9414f9e48598347c558e1a7e4c24db16
Farhan Rahman <frn.rahman@gmail.com>
2021-01-17 13:29:57 -0700

    use of dictionary

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-# Following mosh's lecture 5.18:
+# Following mosh's lecture 5.19:
 
-numbers = [1, 2, 3, 2, 3]
+point = dict(x=1, y=3)
 
-firstSet = set(numbers)
-
-secondSet = {3, 4, 5}
-
-# priting union of two sets
-print(firstSet | secondSet)
-
-print("printing common", firstSet & secondSet)
-
-print("printing either", firstSet ^ secondSet)
+print(point["x"])
+print(point["y"])
