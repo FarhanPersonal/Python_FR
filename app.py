31fbf7477e9d43abb81b8c7244af78f45861a4e9
Farhan Rahman <frn.rahman@gmail.com>
2021-01-16 21:46:24 -0700

    Adding and removing objects from list

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
-for x in range(2, 5):
-    print(x)
+letters = ["a", "b", "c"]
+
+# adding objects to a list
+letters.append("d")
+
+letters.insert(0, "-")
+
+# removing method
+letters.pop()
+
+letters.pop(0)
+
+letters.remove("b")
+
+print(letters)
