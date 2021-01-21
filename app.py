35d3c6f2ce7525201fa38d71047f947c8fa0fc42
Farhan Rahman <frn.rahman@gmail.com>
2021-01-21 12:32:29 -0700

    Ploting 3D surface plot

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
+# https://matplotlib.org/3.1.0/gallery/mplot3d/surface3d.html
 
-array = []
-array.append(['a', 100])
-array.append(['b', 200])
-array.append(['c', 300])
-array.append(['d', 400])
+from matplotlib import pyplot as plt
 
-print(array)
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+import numpy as np
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# Make data.
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+
+# Plot the surface.
+surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# Customize the z axis.
+ax.set_zlim(-1.01, 1.01)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+# Add a color bar which maps values to colors.
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.show()
