ca82744f94eda3c01fa98c8f6d5227fff704915c
Farhan Rahman <frn.rahman@gmail.com>
2021-01-30 11:22:50 -0700

    Creating 3D point cloud from 1D arrays

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
-import numpy
-import numpy
-from scipy import interpolate
 
-x_coord = [0, 1, 0, 1]
-z_coord = [0, 0, 1, 1]
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
 
-y_displ = [1, 7, 5, 1]
+X = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4,
+     4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8]
 
-row_wise_data = tuple(zip(x_coord, z_coord, y_displ))
+Y = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5,
+     6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-
-def get_array_from_tuple(data_tuple, col_indx_in_tuple):
-    data_array = []
-    for i in range(0, len(data_tuple)):
-        data_array.append(data_tuple[i][col_indx_in_tuple])
-
-    return data_array
+Z = [21384, 29976, 15216, 4584, 10236, 7546, 6564, 2844, 4926, 7722, 4980, 2462, 12768, 9666, 2948, 6548, 10776, 8260, 8674, 5584, 5382, 10542, 12544, 5268, 25888, 31220, 9064, 7536, 6618, 1928, 9030, 5790, 6076, 8290, 8692, 4006, 14722, 11016, 2818, 9458, 3054, 5976, 1102, 1084, 9700, 8904, 12510, 11176, 10712, 6548, 2600, 5070,
+     6538, 4514, 1036, 292, 12572, 6534, 4478, 18500, 10452, 1912, 14254, 31050, 3880, 744, 990, 5534, 1670, 446, 2778, 8272, 14726, 27094, 872, 418, 884, 476, 2806, 1246, 1140, 922, 6202, 10848, 28828, 2360, 9660, 1412, 4296, 5272, 2854, 4150, 770, 5628, 4676, 3500, 31220, 10480, 5704, 5550, 1528, 3168, 2092, 2056, 1874, 7312, 938, 7428]
 
 
-# getting back y_displ array:
-x_coord_array = get_array_from_tuple(row_wise_data, 2)
+x = np.reshape(X, (9, 12))
+y = np.reshape(Y, (9, 12))
+z = np.reshape(Z, (9, 12))
 
-print(x_coord_array)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# ax.plot_surface(x, y, z)
+ax.scatter(x, y, z)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
